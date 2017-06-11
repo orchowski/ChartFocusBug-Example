5344a5f2b0ce0a23c55e6c9465a45a5647657be5
Aleksander Orchowski <orchowskialeksander@gmail.com>
2017-06-11 12:21:35 +0200

    initial commit

---
+++ ChartFocusBug/ChartFocusBug/MainPage.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+
+
+namespace ChartFocusBug
+{
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+            this.Loaded += OnLoaded;            
+        }
+
+        private void OnLoaded(object sender, RoutedEventArgs e)
+        {
+            this.chart.DataContext = Enumerable.Range(0, 1000);
+            
+        }
+    }
+}
